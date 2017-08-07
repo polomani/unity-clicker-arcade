d244466e9424ee1e15fb6c56bc3a60f1e018cf2b
polomani <ivano.polomani@gmail.com>
2017-08-07 19:29:17 +0300

    tween transitions added

--- Assets/Scripts/Tween.cs
+++ Assets/Scripts/Tween.cs
@@ -15,13 +15,13 @@ public class Tween : MonoBehaviour{
         return (float) obj.GetType().GetField(property).GetValue(obj);
     }
 
-    private IEnumerator CoroutineTo(Component obj, string property, float to, float duration, Action<float> OnUpdate, Action callback)
+    private IEnumerator CoroutineTo(Component obj, string property, float to, float duration, Func<float, float> transition, Action callback)
     {
         float time = 0;
         while (true)
         {
             time += Time.deltaTime;
-            OnUpdate(time);
+            setValue(obj, property, Mathf.Lerp(getValue(obj, property), to, transition(time/duration)));
             if (time >= duration)
             {
                 if (callback!=null) callback();
@@ -39,24 +39,66 @@ public class Tween : MonoBehaviour{
             time += Time.deltaTime;
             if (time >= duration)
             {
-                callback();
+                if (callback!=null) callback();
                 yield break;
             }
             yield return new WaitForEndOfFrame();
         }
     }
 
+    public static void To(Component obj, string property, float to, float duration)
+    {
+        To(obj, property, to, duration, Transition.LINEAR, null);
+    }
+
     public static void To(Component obj, string property, float to, float duration, Action callback)
+    {
+        To(obj, property, to, duration, Transition.LINEAR, callback);
+    }
+
+    public static void To(Component obj, string property, float to, float duration, Func<float,float> transition, Action callback)
     {
         Tween tween = Utils.GetOrAddComponent<Tween>(obj);
         tween.StartCoroutine(tween.CoroutineTo(obj, property, to, duration, 
-            (time)=>tween.setValue(obj, property, Mathf.Lerp(tween.getValue(obj, property), to, time / duration)),
+            transition,
             callback));
+    }
+
+    public static void Delay(Component obj, float duration)
+    {
+        Delay(obj, duration, null);
     }
 
     public static void Delay(Component obj, float duration, Action callback)
     {
         Tween tween = Utils.GetOrAddComponent<Tween>(obj);
         tween.StartCoroutine(tween.CoroutineDelay(duration, callback));
+    }
+}
+
+public static class Transition {
+
+    public static float LINEAR (float t) {
+        return t;
+    }
+
+    public static float EASE_IN(float t)
+    {
+        return 1f - Mathf.Cos(t * Mathf.PI * 0.5f);
+    }
+
+    public static float EASE_OUT(float t)
+    {
+        return Mathf.Sin(t * Mathf.PI * 0.5f);
+    }
+
+    public static float SMOOTH_STEP(float t)
+    {
+        return t * t * (3f - 2f * t);
+    }
+
+    public static float SMOOTHER_STEP(float t)
+    {
+        return t*t*t * (t * (6f*t - 15f) + 10f);
     }
 }
